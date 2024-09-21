dd0f95d2c19574a73b3a41955028019d037df73e
AdarshSavalagi <21ai002@sitmng.ac.in>
2024-09-21 11:14:24 +0530

    api end point update

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,4 +1,4 @@
 export const tokenName='_token';
 export const isAdminName = '_isAdmin';
-export const apiEndPoint = 'http://localhost:8080/api/v1';
+export const apiEndPoint = 'http://sitmanagement.adarshsavalagi.me/api/v1';
 export const appName = 'SIT College Management ';
